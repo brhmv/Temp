371ab6607e6d1e6be47596cf944c282558da53ce
brhmv <allahverdiibrahimov2004@gmail.com>
2023-03-24 21:39:00 +0400

    first commit

---
+++ Boss.az.wpf/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Boss.az.wpf.View;
+using Boss.az.wpf.ViewModels;
+using Boss.az.wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Boss.az.wpf
+{
+    public partial class App : Application
+    {
+        public static MainView? window { get; set; }
+
+
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            window = new MainView
+            {
+                DataContext = new MainViewModel()                
+            };
+            window.Content_Control.Content = new WorkerOrEmployer();
+
+
+
+            SignInUp siu = new();
+            siu.SignInUpHolderControl.Content = new SignIn();
+
+
+            window.Show();
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Boss.az.wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,103 @@
+﻿using Boss.az.wpf.View;
+using Boss.az.wpf.Views;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Boss.az.wpf.ViewModels
+{
+    internal class MainViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string? propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public UserControl? _SignInUpHolderControlPro;
+        public UserControl? SignInUpHolderControlPro
+        {
+            get { return _UC; }
+            set { _UC = value; OnPropertyChanged(nameof(SignInUpHolderControlPro)); }
+        }
+
+        UserControl? _UC;
+        public UserControl? CurrentViewModel
+        {
+            get { return _UC; }
+            set { _UC = value; OnPropertyChanged(nameof(CurrentViewModel)); }
+        }
+
+
+        public string? User { get; set; }
+
+
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+
+
+        public MainViewModel()
+        {
+        }
+
+        public RelayCommand UserIsWorker
+        {
+            get => new RelayCommand(() =>
+        {
+            User = "Worker";
+
+            App.window.Content = new SignInUp();
+        });
+        }
+
+        public RelayCommand UserIsEmployer
+        {
+            get => new RelayCommand(() =>
+            {
+                User = "Employer";
+            });
+        }
+
+        public RelayCommand SignUpButton
+        {
+            get => new RelayCommand(() =>
+            {
+                SignInUpHolderControlPro = new SignUp();
+
+            });
+        }
+
+        public RelayCommand SignInButton
+        {
+            get => new RelayCommand(() =>
+            {
+                SignInUpHolderControlPro = new SignIn();
+
+
+            });
+        }
+
+
+        public RelayCommand SignIn_Button_Click
+        {
+            get => new RelayCommand(() =>
+            {
+                SignIn si = new();
+                //si.SignIn_Button.IsEnabled= true;
+
+            });
+        }
+
+
+    }
+}
